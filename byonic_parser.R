1316f378645cef417924dd24feff587957172ac9
anupshah14 <14209913+anupshah14@users.noreply.github.com>
2017-12-19 16:36:48 +1100

    added R scripts

---
+++ byonic_parser.R
@@ -0,0 +1,79 @@
+## ==== Script to parse byonic excel output
+## Intput the main directory
+## The r script should run through the parent directory
+directory<- choose.dir()
+
+library("rstudioapi")
+library("tidyverse")
+library("data.table")
+library("readxl")
+library("xlsx")
+
+setwd(directory)
+getwd()
+### Set working directory
+#current_path <- getActiveDocumentContext()$path 
+# The next line set the working directory to the relevant one:
+#setwd(dirname(current_path ))
+
+source("D:/Applications/Scripts/Byonic/functions_byonic_parser.R", chdir = TRUE)
+
+
+
+#sanity check
+getwd()
+# Get all file names with byonic output
+file_names<-list.files(getwd(),pattern = "*_Byonic.xlsx",recursive = T)
+## clean up file names
+sheet_names<- file_names %>% gsub(".*[/]","",.) %>% gsub("[.]raw.*","",.) %>% sub("^[^_]*_","",.)
+
+#fiile_names[1]
+
+# Read a sheet named "Protein" from byonic output file
+## Use other library "readxl" as others (openxl and tidyverse function gave error
+## Need to store column headers as byonic output headers are messy
+column_headers<-c("Protein_Rank","Description","Log_Probability","Best_Log_Probability","Best_Score","Total_intensity",
+                  "Number_of_Spectra", "Number_of_Unique_Peptides", "Number_of_Modified_Peptides",
+                  "Coverage","Numbers_of_AA", 
+                  "Protein_DB_number")
+
+## Recursively read all files
+file_list<-lapply(file_names,read_excel,sheet = "Proteins",col_names = TRUE)
+# Improtant step
+new_list<-lapply(file_list,setNames,column_headers)
+#str(new_list)
+## Copy protein attributes to the blank row below for proteins in group (sharing same protein Rank)
+## use protein_group interation function and apply over list of dataframes
+
+modified_files<- lapply(new_list,function(x) protein_group_iteration(x))
+
+### Count how many proteins per group and store it in "Proteins_in_group" column
+## Use function count_protein_groups
+
+sheets_with_protein_group<-lapply(modified_files,count_protein_groups)
+
+
+# Rename columns based on experiment structure
+## Add suffix to each column (each dataframe) and exlude "Description" column as it is required for merging
+## Use function "change_column_name"
+
+modified_sheets<-map2(sheets_with_protein_group,sheet_names,change_column_names)
+
+## Main merging function
+combined_results<- modified_sheets %>% reduce(merge,by="Description",all.x=TRUE,all.y=TRUE)
+# requires all.x and all.y to be true
+
+## clean the output
+## remove reverse sequences
+## get three main set of columns (Description, Number of spectra and how many number of proteins in Group
+
+
+clean_output<-combined_results %>% 
+  filter(!grepl("^>Reverse",Description)) %>% #worked
+  select(.,"Description",starts_with("Number_of_Spectra"),starts_with("Proteins_in_Group")) %>%
+  arrange(desc(.[[2]])) # sort by second column (Important syntax)
+## Reorder columns alphabetically
+clean_output %>% select(order(colnames(.)))
+write.xlsx(clean_output,"Summary_byonic.xlsx",sheetName = "combined_results",showNA = FALSE, row.names = FALSE)
+
+sessionInfo()
---
+++ functions_byonic_parser.R
@@ -0,0 +1,24 @@
+change_column_names<-function(df,names_vector){
+  data.table::setnames(df,names(df)[-2],paste0(names(df)[-2],"_",names_vector))
+}
+
+count_protein_groups <- function(df){
+ new_df<- df %>% group_by(Protein_Rank) %>%
+    mutate(Proteins_in_Group=n())
+ return(new_df)
+}
+
+protein_group_iteration<- function(df){
+  updated_sheet<- df %>% group_by(Protein_Rank)%>% 
+    mutate(Log_Probability=unique(Log_Probability[!is.na(Log_Probability)]),
+           Best_Log_Probability=unique(Best_Log_Probability[!is.na(Best_Log_Probability)]),
+           Best_Score=unique(Best_Score[!is.na(Best_Score)]),
+           Total_intensity=unique(Total_intensity[!is.na(Total_intensity)]),
+           Number_of_Spectra=unique(Number_of_Spectra[!is.na(Number_of_Spectra)]),
+           Number_of_Unique_Peptides=unique(Number_of_Unique_Peptides[!is.na(Number_of_Unique_Peptides)]),
+           Number_of_Modified_Peptides=unique(Number_of_Modified_Peptides[!is.na(Number_of_Modified_Peptides)]),
+           Coverage=unique(Coverage[!is.na(Coverage)]),
+           Numbers_of_AA=unique(Numbers_of_AA[!is.na(Numbers_of_AA)]),
+           Protein_DB_number=unique(Protein_DB_number[!is.na(Protein_DB_number)]))
+  return(updated_sheet)
+}
